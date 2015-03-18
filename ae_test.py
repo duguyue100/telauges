98e8ffbecc8869ae4d933ec05068e39ef09f925b
Yuhuang Hu <duguyue100@gmail.com>
2015-03-18 23:17:37 +0800

    update denoising auto-encoder

---
+++ ae_test.py
@@ -0,0 +1,90 @@
+"""
+@author: Yuhuang Hu
+@contact: duguyue100@gmail.com
+
+@note: test for auto-encoder
+"""
+
+import numpy as np;
+import theano;
+import theano.tensor as T;
+import matplotlib.pyplot as plt;
+
+import telauges.utils as utils;
+from telauges.hidden_layer import AutoEncoder;
+
+n_epochs=50;
+training_portion=1;
+batch_size=20;
+
+datasets=utils.load_mnist("data/mnist.pkl.gz");
+rng=np.random.RandomState(23455);
+
+### Loading and preparing dataset
+train_set_x, train_set_y = datasets[0];
+valid_set_x, valid_set_y = datasets[1];
+test_set_x, test_set_y = datasets[2];
+    
+n_train_batches=int(train_set_x.get_value(borrow=True).shape[0]*training_portion);
+n_valid_batches=valid_set_x.get_value(borrow=True).shape[0];
+n_test_batches=test_set_x.get_value(borrow=True).shape[0];
+    
+n_train_batches /= batch_size; # number of train data batches
+n_valid_batches /= batch_size; # number of valid data batches
+n_test_batches /= batch_size;  # number of test data batches
+
+print "[MESSAGE] The data is loaded"
+print "[MESSAGE] Building model"
+
+X=T.matrix("X");
+y=T.ivector("y");
+index=T.lscalar();
+
+ae=AutoEncoder(rng=rng,
+               data_in=X,
+               n_vis=784,
+               n_hidden=500,
+               encode_activate_mode="relu",
+               decode_activate_mode="sigmoid");
+               
+
+    
+cost, updates=ae.get_updates(learning_rate=0.1,
+                             corruption_level=0.3);
+
+    
+train_model = theano.function(inputs=[index],
+                              outputs=cost,
+                              updates=updates,
+                              givens={X: train_set_x[index * batch_size: (index + 1) * batch_size]});
+                                      
+print "[MESSAGE] The model is built";
+print "[MESSAGE] Start training"
+
+filters=ae.encode_layer.W.get_value(borrow=True);
+
+for i in xrange(100):
+  plt.subplot(10, 10, i);
+  plt.imshow(np.reshape(filters[:,i], (28, 28)), cmap = plt.get_cmap('gray'), interpolation='nearest');
+  plt.axis('off')
+plt.show();
+
+
+epoch = 0;
+while (epoch < n_epochs):
+  epoch = epoch + 1;
+  c = []
+  for batch_index in xrange(n_train_batches):
+    c.append(train_model(batch_index))
+
+  print 'Training epoch %d, cost ' % epoch, np.mean(c);
+  
+filters=ae.encode_layer.W.get_value(borrow=True);
+for i in xrange(100):
+  plt.subplot(10, 10, i);
+  plt.imshow(np.reshape(filters[:,i], (28, 28)), cmap = plt.get_cmap('gray'), interpolation='nearest');
+  plt.axis('off')
+plt.show();
+
+  
+  
--- telauges/hidden_layer.py
+++ telauges/hidden_layer.py
@@ -8,6 +8,7 @@
 import numpy as np;
 import theano;
 import theano.tensor as T;
+from theano.tensor.shared_randomstreams import RandomStreams;
 
 import telauges.nnfuns as nnf;
 
@@ -102,6 +103,9 @@ class HiddenLayer(object):
     """
     
     return self.activation(s);
+  
+  def get_output(self, x):
+    return self.get_activation(self.get_pre_activation(x));
 
 class SoftmaxLayer(HiddenLayer):
   """
@@ -141,3 +145,98 @@ class SoftmaxLayer(HiddenLayer):
     """
     
     return T.mean(T.neq(self.prediction, y));
+  
+class AutoEncoder(object):
+  """
+  An implementation of naive Auto-encoder
+  """
+  
+  def __init__(self,
+               rng,
+               data_in,
+               n_vis,
+               n_hidden,
+               encode_activate_mode="tanh",
+               decode_activate_mode="tanh"):
+    """
+    Init a naive Auto-encoder without bounding weights
+    """
+    
+    self.rng=rng;
+    self.theano_rng=RandomStreams(rng.randint(2 ** 30));
+    self.input=data_in;
+    self.n_vis=n_vis;
+    self.n_hidden=n_hidden;
+    
+    self.encode_activate_mode=encode_activate_mode;
+    self.decode_activate_mode=decode_activate_mode;
+    
+    self.encode_layer=HiddenLayer(rng=self.rng,
+                                  data_in=self.input,
+                                  n_in=self.n_vis,
+                                  n_out=self.n_hidden,
+                                  activate_mode=self.encode_activate_mode);
+    
+    self.decode_layer=HiddenLayer(rng=self.rng,
+                                  data_in=self.encode_layer.output,
+                                  n_in=self.n_hidden,
+                                  n_out=self.n_vis,
+                                  activate_mode=self.decode_activate_mode);
+                                  
+    self.parms=self.encode_layer.params;
+                                  
+  def get_feature(self, x):
+    return self.encode_layer.get_output(x);
+  
+  def get_decode_from_feature(self, x):
+    return self.decode_layer.get_output(x);
+  
+  def get_corruption_input(self,
+                           x,
+                           corruption_level):
+    return self.theano_rng.binomial(size=x.shape, n=1,
+                                    p=1 - corruption_level,
+                                    dtype="float32") * x;
+  
+  def get_cost(self, x, y):
+    return -T.sum(x * T.log(y) + (1-x)* T.log(1 - y), axis=1);
+  
+  def get_updates(self,
+                  learning_rate,
+                  corruption_level=None,
+                  L1_rate=0.000,
+                  L2_rate=0.000,):
+    
+    if corruption_level is not None:
+      x=self.get_corruption_input(self.input, corruption_level);
+      y=self.decode_layer.get_output(self.encode_layer.get_output(x));
+    else:
+      y=self.decode_layer.output;
+      
+    #cost=T.sum(T.pow(T.sub(self.decode_layer.output, x),2), axis=1);
+    
+    cost=self.get_cost(self.input, y);    
+    cost=T.mean(cost);
+    
+    params=self.encode_layer.params+self.decode_layer.params;
+    gparams=T.grad(cost, params);
+
+    updates=[(param_i, param_i-0.1*grad_i)
+             for param_i, grad_i in zip(params, gparams)];
+             
+    return cost, updates;
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
