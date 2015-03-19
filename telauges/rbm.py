137a5bbfc532626f268431ed6a49ae178c14e2aa
Yuhuang Hu <duguyue100@gmail.com>
2015-03-19 19:23:53 +0800

    small update on rbm

--- telauges/rbm.py
+++ telauges/rbm.py
@@ -27,13 +27,31 @@ class RBM(object):
                n_hidden,
                W=None,
                v_bias=None,
-               h_bias=None):
+               h_bias=None,
+               activate_mode="tanh"):
     
     self.rng=rng;
     self.theano_rng=RandomStreams(self.rng.randint(2**30));
     self.input=data_in;
     self.n_vis=n_vis;
     self.n_hidden=n_hidden;
+    self.activate_mode=activate_mode;
+    
+    if (self.activate_mode=="tanh"):
+      self.activation=nnf.tanh;
+    elif (self.activate_mode=="relu"):
+      self.activation=nnf.relu;
+    elif (self.activate_mode=="sigmoid"):
+      self.activation=nnf.sigmoid;
+    elif (self.activate_mode=="softplus"):
+      self.activation=nnf.softplus;
+    elif (self.activate_mode=="softmax"):
+      self.activation=nnf.softmax;
+    elif (self.activate_mode=="linear"):
+      self.activation=nnf.linear;
+    else:
+      raise ValueError("Value %s is not a valid choice of activation function"
+                       % self.activate_mode);
     
     if W is None:
       initial_W = np.asarray(rng.uniform(low=-4 * np.sqrt(6. / (n_hidden + n_vis)),
@@ -58,4 +76,18 @@ class RBM(object):
     
     self.params=[self.W, self.v_bias, self.h_bias];
     
+  def prop_up(self,
+              vis):
+    
+    pre_activation=T.dot(vis, self.W)+self.h_bias;
+    
+    return [pre_activation, self.activation(pre_activation)];
+  
+  def prop_down(self,
+                hidden):
+    
+    pre_activation=T.dot(hidden, self.W.T)+self.v_bias;
+    
+    return [pre_activation, self.activation(pre_activation)];
+  
   
